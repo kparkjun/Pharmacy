b1ffe6cb8f4717b13504e40bfa6d21076018914b
kparkjun <aparkjun@naver.com>
2025-05-23 03:19:17 +0900

    first

---
+++ src/main/java/com/example/project/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.project.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+
+    private MetaDto meta;
+
+    private List<DocumentDto> documentList;
+}
--- src/test/java/com/example/project/ProjectApplicationTests.java
+++ src/test/java/com/example/project/ProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fastcampus.project;
+package com.example.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
